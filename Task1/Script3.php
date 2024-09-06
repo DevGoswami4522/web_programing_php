b7553ae2e3d513e172d54c29e99e4b7c59bfa7c1
DevGoswami4522 <goswamidev41@gmail.com>
2024-09-06 11:09:56 +0530

    Script3.php

--- Task1/Script3.php
+++ Task1/Script3.php
@@ -1,8 +1,8 @@
 <!-- Script 3: Calculate and display the sum of two variables $num1 and $num2 -->
 
 <?php
-$num1 = 120;
-$num2 = 45;
-$sum = $num1 + $num2;
+$num1 = 11;
+$num2 = 11;
+$sum = $num1  $num2;
 echo "The sum of $num1 and $num2 is $sum.";
 ?>
