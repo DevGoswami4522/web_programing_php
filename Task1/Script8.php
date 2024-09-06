b391c43407b2a12723abeb4c510a30abe26aa4e6
DevGoswami4522 <goswamidev41@gmail.com>
2024-09-06 11:19:36 +0530

    Update Script8.php

--- Task1/Script8.php
+++ Task1/Script8.php
@@ -1,6 +1,5 @@
-<!-- Script 8: Declare Variables for a person's name,age and city. -->
 <?php
-$name = "Tarak";
-$age = 36;
-$city = "Mumbai";
+  $name = "goa";
+      $age = 21;
+  $city = "rajkot";  
 ?>
