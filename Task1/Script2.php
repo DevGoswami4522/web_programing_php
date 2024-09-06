130314156462e28e14ab2fab9b383856abb2b3d1
DevGoswami4522 <goswamidev41@gmail.com>
2024-09-06 11:04:39 +0530

    Script2.php

--- Task1/Script2.php
+++ Task1/Script2.php
@@ -1,7 +1,7 @@
 <!-- Script 2: Display a message with variables $name and $age -->
 
 <?php
-$name = "Nishant";
-$age = 20;
+    $name = "dev";
+    $age = 22;
 echo "My name is $name and I am $age years old.";
 ?>
